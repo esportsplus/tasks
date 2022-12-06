807524278eb85c333aa33978348a6ca1f47313dc
Israel Casillas Jr <ICJR@users.noreply.github.com>
2022-12-05 22:37:31 -0800

    feat: allow creation of multiple instances

--- src/raf.ts
+++ src/raf.ts
@@ -2,7 +2,7 @@ import { Fn } from './types';
 import factory from './factory';
 
 
-export default factory(
+export default () => factory(
     window.requestAnimationFrame || function (fn: Fn) {
         return window.setTimeout(fn, (1000 / 60));
     }
